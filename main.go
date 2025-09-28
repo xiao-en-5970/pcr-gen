ce0340a1877da225feabdb0d706ce52f931776ea
fuyizhong <fuyizhong@dp.tech>
2025-09-28 16:39:24 +0800

    相邻两帧，后者修正至前者渲染帧+0，防止个别角色卡帧，用set顺序硬排列

--- main.go
+++ main.go
@@ -213,7 +213,7 @@ func (s *Service) Gen() {
 					panic(err)
 				}
 				if i > 0 && s.Axis[i-1].Operation == "连点" && axis.Operation == "连点" && aftF-preF <= 60 {
-					fix += (preF - aftF) + 5 + 60 //两帧差+短间隔+提前量修正
+					fix += (preF - aftF) + 0 + 60 //两帧差+短间隔+提前量修正
 				} else {
 					fix = 0
 				}
